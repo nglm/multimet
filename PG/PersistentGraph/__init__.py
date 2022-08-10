29df0411fbedb2a160ccdab96e84c690fe43ea7f
Natacha <nglm@protonmail.com>
2022-08-10 14:18:14 +0200

    reorganise folders

--- PG/PersistentGraph/__init__.py
+++
@@ -1,9 +0,0 @@
-# To replace 'from PersistentGraph.component import Component'
-# And use from PersistentGraph import Component'
-
-from .component import Component
-from .vertex import Vertex
-from .edge import Edge
-from .persistentgraph import PersistentGraph
-
-
--- PG/Vis/__init__.py
+++
@@ -1,7 +0,0 @@
-# To replace 'from .vertexstyle import VertexStyle'
-# And use from Vis import VertexStyle'
-from .componentstyle import ComponentStyle
-from .vertexstyle import VertexStyle
-from .edgestyle import EdgeStyle
-from .uncertaintystyle import UncertaintyStyle
-from .pgraphstyle import PGraphStyle
